1879fe901cb85dc342f9fa0ef0f73d7603edb71b
Jan Grünewald <gruenewald@cominto.de>
2015-06-06 09:41:58 +0200

    removed copyright notices from versioning update copyright changed build tools version in build.gradle

--- app/src/main/java/de/gruenewald/udacity/spotifystreamer/MainActivity.java
+++ app/src/main/java/de/gruenewald/udacity/spotifystreamer/MainActivity.java
@@ -1,3 +1,26 @@
+/*******************************************************************************
+ Copyright (c) 2015 by Jan Grünewald.
+ jan.gruenewald84@googlemail.com
+ -------------------------------------------------------------------------------
+ This file is part of 'Spotify Streamer'. 'Spotify Streamer' was developed as
+ part of the Android Developer Nanodegree by Udacity. For further
+ information see:
+ https://www.udacity.com/course/android-developer-nanodegree--nd801
+ -------------------------------------------------------------------------------
+ 'Spotify Streamer' is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ 'Spotify Streamer' is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with 'Spotify Streamer'.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package de.gruenewald.udacity.spotifystreamer;
 
 import android.support.v7.app.ActionBarActivity;
